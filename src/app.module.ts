d63d64f8f3f20b2e82c542b57eb14731cb7aef2c
Lucas <lucaslops12@gmail.com>
2025-07-24 10:04:43 -0300

    feat: integrate dotenv-cli for development environment and switch to Fastify
    - Updated start:dev script to use dotenv-cli for loading environment variables.
    - Switched main application to use FastifyAdapter for improved performance.
    - Enhanced EnvConfigModule to properly load environment variables from .env files.
    - Updated unit tests for EnvConfigService to validate environment variable retrieval.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,7 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
-import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
-import { join } from 'path';
 
 @Module({
   imports: [EnvConfigModule],
 })
-export class AppModule extends ConfigModule {
-  static async forRoot(
-    options: ConfigModuleOptions = {},
-  ): Promise<DynamicModule> {
-    return super.forRoot({
-      ...options,
-      envFilePath: [
-        join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
-      ],
-    });
-  }
-}
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { EnvConfigService } from './shared/infrastructure/env-config/env-config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const envConfigService = app.get(EnvConfigService);
+
+  const port = envConfigService.getAppPort() || 3001;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
