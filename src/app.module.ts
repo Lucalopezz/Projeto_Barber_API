4cb2bbabd2d34f802f6a4b91c68e737a989cb7fb
Lucas <lucaslops12@gmail.com>
2025-07-24 10:19:12 -0300

    feat: add Users module with controller, service, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
+import { UsersModule } from './users/infrastructure/users.module';
 
 @Module({
-  imports: [EnvConfigModule],
+  imports: [EnvConfigModule, UsersModule],
 })
 export class AppModule {}
