a41f9f0ef4b7b144657d3ce6432150501d2ba558
Lucas <lucaslops12@gmail.com>
2025-07-25 10:31:47 -0300

    feat: implement create user use case

--- src/users/application/usecases/create-user.usecase.ts
+++ src/users/application/usecases/create-user.usecase.ts
@@ -1,7 +1,11 @@
 import { UseCaseContract } from '@/shared/application/usecases/use-case';
-import { UserOutput } from '../dtos/user-output.dto';
+import { UserOutput, UserOutputMapper } from '../dtos/user-output.dto';
 import { Role } from '@/users/domain/entities/role.enum';
 import { BadRequestError } from '@/shared/application/errors/bad-request-error';
+
+import { HashProvider } from '@/shared/application/providers/hash-provider';
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { UserEntity } from '@/users/domain/entities/user.entity';
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace CreateUseCase {
@@ -14,13 +18,27 @@ export namespace CreateUseCase {
   export type Output = UserOutput;
 
   export class UseCase implements UseCaseContract<Input, Output> {
-    execute(input: Input): Promise<UserOutput> {
+    constructor(
+      private readonly hashProvider: HashProvider,
+      private userRepository: UserRepository.Repository,
+    ) {}
+    async execute(input: Input): Promise<UserOutput> {
       const { email, password, role, name } = input;
 
       if (!email || !password || !name || !role) {
         throw new BadRequestError('Input data not provided');
       }
+      await this.userRepository.emailExists(email);
+      const hashPass = await this.hashProvider.generateHash(password);
+
+      const entity = new UserEntity(
+        Object.assign(input, {
+          password: hashPass,
+        }),
+      );
+      await this.userRepository.insert(entity);
+
+      return UserOutputMapper.toOutput(entity);
     }
-    // Create user logic
   }
 }
