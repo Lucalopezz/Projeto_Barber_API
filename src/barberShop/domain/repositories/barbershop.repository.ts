809a46ca3976f51aa502fca4279cc4f603ea1674
Lucas <lucaslops12@gmail.com>
2025-08-05 19:37:14 -0300

    feat: implement BarberShop repository with search parameters and results

---
+++ src/barberShop/domain/repositories/barbershop.repository.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+
+import {
+  SearchableRepositoryInterface,
+  SearchParams,
+  SearchResult,
+} from '@/shared/domain/repositories/searchable.repository';
+import { BarberShopEntity } from '../entities/barber-shop.entity';
+
+export namespace BarberShopRepository {
+  export type Filter = string;
+
+  export class BarberShopSearchParams extends SearchParams<Filter> {}
+
+  export class BarberShopSearchResult extends SearchResult<
+    BarberShopEntity,
+    Filter
+  > {}
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export interface Repository
+    extends SearchableRepositoryInterface<
+      BarberShopEntity,
+      Filter,
+      BarberShopSearchParams,
+      BarberShopSearchResult
+    > {}
+}
