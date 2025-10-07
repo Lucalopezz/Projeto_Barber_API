0dd9d85cb1572bf9c61aa15741e591eb04870515
Lucas <lucaslops12@gmail.com>
2025-10-07 17:03:10 -0300

    feat: create ServicesRepository with ServiceEntity interface implementation

---
+++ src/services/domain/repositories/services.repository.ts
@@ -0,0 +1,8 @@
+import { ServiceEntity } from '../entities/services.entity';
+import { RepositoryInterface } from '@/shared/domain/repositories/repository-contract';
+
+/* eslint-disable @typescript-eslint/no-namespace */
+export namespace ServicesRepository {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export interface Repository extends RepositoryInterface<ServiceEntity> {}
+}
