7c5c5de19200cc432ebe345b8e6b8be9154999b1
Lucas <lucaslops12@gmail.com>
2025-10-21 16:03:38 -0300

    feat: add appointments nest module and controller

---
+++ src/appointments/infrastructure/appointments.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CreateAppointmentDto } from './dto/create-appointment.dto';
+import { UpdateAppointmentDto } from './dto/update-appointment.dto';
+
+@Controller('appointments')
+export class AppointmentsController {
+  @Post()
+  create(@Body() createAppointmentDto: CreateAppointmentDto) {
+    //return this.appointmentsService.create(createAppointmentDto);
+  }
+
+  @Get()
+  findAll() {
+    //return this.appointmentsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    //return this.appointmentsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateAppointmentDto: UpdateAppointmentDto,
+  ) {
+    //return this.appointmentsService.update(+id, updateAppointmentDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    //return this.appointmentsService.remove(+id);
+  }
+}
