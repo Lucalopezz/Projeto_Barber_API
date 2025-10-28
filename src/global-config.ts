9401c57430280550ac61d585e3d2631525e6e26a
Lucas <lucaslops12@gmail.com>
2025-10-28 08:58:30 -0300

    feat: implement wrapper data interceptor to padronize the returns

--- src/global-config.ts
+++ src/global-config.ts
@@ -4,6 +4,7 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { WrapperDataInterceptor } from './shared/infrastructure/interceptors/wrapper-data/wrapper-data.interceptor';
 
 export function applyGlobalConfig(app: INestApplication) {
   app.useGlobalPipes(
@@ -14,5 +15,8 @@ export function applyGlobalConfig(app: INestApplication) {
       transform: true,
     }),
   );
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalInterceptors(
+    new WrapperDataInterceptor(),
+    new ClassSerializerInterceptor(app.get(Reflector)),
+  );
 }
